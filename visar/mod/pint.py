a92ebd143fd8721974f065b8fe89cbfd7205c825
TWoolhouse <thomasrwoolhouse@gmail.com>
2024-12-04 16:56:19 +0000

    support just the unit type without a quantity

--- visar/mod/pint.py
+++ visar/mod/pint.py
@@ -7,16 +7,25 @@ def g_mag(v: pint.Quantity) -> float:
     return float(v.magnitude)
 
 
-def p_dimensions(v: pint.Quantity) -> Segment:
-    return "unit", f"{v.units:D}"
+def p_dimensions(u: pint.Unit) -> Segment:
+    return "unit", f"{u:D}"
 
 
-def p_dimensions_base(v: pint.Quantity) -> Segment:
-    return "SI", f"{v.to_base_units().units:D}"
+def p_dimensions_base(u: pint.Unit) -> Segment:
+    return "SI", f"{(1 * u).to_base_units().units:D}"
+
+
+def g_unit(v: pint.Quantity) -> pint.Unit:
+    return v.units
 
 
 def enable(visar: Ar) -> None:
     visar.register(
+        pint.Unit,
+        (g_instance(pint.Unit), [p_dimensions, p_dimensions_base]),
+    ).inject(visar.special.any).at_tail()
+
+    visar.register(
         pint.Quantity,
-        (g_instance(pint.Quantity), [p_dimensions, p_dimensions_base, n_into(g_mag, visar[float])]),
+        (g_instance(pint.Quantity), [n_into(g_unit, visar[pint.Unit]), n_into(g_mag, visar[float])]),
     ).inject(visar.special.any).at_tail()
